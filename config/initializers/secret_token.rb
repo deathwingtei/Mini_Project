7c8a2b045333d534e242b3e408df83f029ff03d6
Deathwing <deathwing@deathwing-Inspiron-N5010.(none)>
2013-02-03 15:42:04 +0700

    Create Mini Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniProject::Application.config.secret_token = '7799b5cb29c11e34bcaec0c91aeefa90735a4af3a21f79377e29dbdbea672ec81724a1a152aa5fcc12d930f503cf3877821c5a5b304013c05baca69fa074f5a0'
