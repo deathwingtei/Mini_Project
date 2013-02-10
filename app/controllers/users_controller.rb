996076ceb853498418314feb2b4b41e1e8639a90
Deathwing <deathwing@deathwing-Inspiron-N5010.(none)>
2013-02-10 21:34:01 +0700

    Sign in and show user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+
+	def index #first index to show
+		@users = User.all
+	end
+
+	def new
+		@user = User.new #create new user
+	end
+
+	def show #show for user
+		@user = User.find(params[:id])
+	end
+
+	def create # to create something
+		@user = User.new(params[:user])  #create user
+		if @user.save #if cansave
+			flash[:success] = "Welcome to the Demo App!"
+			redirect_to @user
+		else
+			render 'new' #if can't save show new
+		end
+	end
+
+	def edit # for edit something
+  		@user = User.find(params[:id])
+	end
+
+	def update # for update something
+		@user = User.find(params[:id])
+		    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+	end
+
+	def destroy # for destroy something
+		@user = User.destroy(params[:id])
+	end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  firstname       :string(255)
+#  lastname        :string(255)
+#  email           :string(255)
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :firstname, :lastname, :password, :password_confirmation
+  has_secure_password
+  before_save { |user| user.email = email.downcase }
+
+  validates :firstname, presence: true, length: { maximum: 50 }
+  validates :lastname, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+end
